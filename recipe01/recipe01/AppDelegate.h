4776118aac8fb1218db407d10c1d564b9f959e69
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2011-12-17 19:30:47 +0900

    init recipe01

---
+++ recipe01/recipe01/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  recipe01
+//
+//  Created by 畠山 貴 on 11/12/17.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ recipe01/recipe01/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  recipe01
+//
+//  Created by 畠山 貴 on 11/12/17.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
